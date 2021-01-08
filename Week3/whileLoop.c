6ef95da330d20eec581622e43b4a89359297ed3e
kanghee <kanghee.cho7@gmail.com>
2021-01-07 20:28:20 -0800

    January 7th commit

---
+++ Week3/whileLoop.c
@@ -0,0 +1,15 @@
+/*  Simple While loop
+    Kanghee Cho
+    January 7 2021
+*/
+
+#include<stdio.h>
+
+int main() {
+    int i = 0;
+    while(i < 10) {
+        printf("i = %d\n",i);
+        i++;
+    }
+    return 0;
+}
---
+++ Week3/whleCntChar.c
@@ -0,0 +1,28 @@
+/*  While-Cnt-Char
+    Kanghee Cho
+    January 7th 2021
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int blanks = 0, digits = 0, letters = 0, others = 0;
+    int c; //use for int value of character
+    while ((c = getchar()) != EOF) {
+        if (c == ' '){
+            ++blanks;
+        }
+        else if (c >= '0' && c <= '9') {
+            ++digits;
+        } 
+        else if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'){
+            ++letters;
+        }
+        else {
+            ++others;
+        }
+    }
+    printf("blanks = %d, digits = %d, letters = %d, others = %d\n\n", 
+    blanks, digits, letters, others);
+    return 0;
+}
